aebc99ee4fa4898a8fe24cdf0daf4d4346d39a0d
mazayasaa <aya.nurkholik@gmail.com>
2020-12-04 06:11:58 +0700

    --

--- app/src/main/java/com/vokasi/dzikiryuk/MenuTasbih.java
+++ app/src/main/java/com/vokasi/dzikiryuk/MenuTasbih.java
@@ -3,13 +3,25 @@ package com.vokasi.dzikiryuk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MenuTasbih extends AppCompatActivity {
+    private TextView mShowCount;
+    private int mCount=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasbih);
+        mShowCount=findViewById(R.id.countView);
 
+    }
+
+    public void hitung(View view) {
+        mCount++;
+        if(mShowCount!=null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
